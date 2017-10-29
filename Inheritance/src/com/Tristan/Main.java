680c4f08d511bcc876b786cd3cb3c3ae06279a47
Tristan Southwell <tristan.southwell@hotmail.com>
2017-10-29 12:13:45 +1100

    inhertiance

--- Inheritance/src/com/Tristan/Main.java
+++ Inheritance/src/com/Tristan/Main.java
@@ -3,6 +3,12 @@ package com.Tristan;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Animal animal = new Animal("animal1", 1, 1, 5, 5);
+        Dog dog = new Dog("Huskie", 8, 20, 2, 4, 1, 20, "Selfish");
+
+        dog.eat();
+//        dog.walk();
+        dog.run();
     }
 }
---
+++ Inheritance/src/com/Tristan/fish.java
@@ -0,0 +1,15 @@
+package com.Tristan;
+
+public class fish extends Animal {
+
+    private int gills;
+    private int eyes;
+    private int fins;
+
+    public fish(String name, int brain, int body, int size, int weight, int gills, int eyes, int fins) {
+        super(name, brain, body, size, weight);
+        this.gills = gills;
+        this.eyes = eyes;
+        this.fins = fins;
+    }
+}
