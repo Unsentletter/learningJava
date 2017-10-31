844009e3777a8894f40b55a10c48e3a264977c0c
Tristan Southwell <tristan.southwell@hotmail.com>
2017-10-31 21:18:18 +1100

    Composition

---
+++ Composition/src/com/Tristan/Vehicle.java
@@ -0,0 +1,9 @@
+package com.Tristan;
+
+public class Vehicle {
+    private String name;
+
+    public Vehicle(String name) {
+        this.name = name;
+    }
+}
---
+++ CompositionChallenge/src/com/Tristan/Ceiling.java
@@ -0,0 +1,20 @@
+package com.Tristan;
+
+public class Ceiling {
+
+    private int height;
+    private int paintColor;
+
+    public Ceiling(int height, int paintColor) {
+        this.height = height;
+        this.paintColor = paintColor;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getPaintColor() {
+        return paintColor;
+    }
+}
