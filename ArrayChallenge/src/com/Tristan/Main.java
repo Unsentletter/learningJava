e63ded15590202102b7f61a4382779bf0dcbe7bd
Tristan Southwell <tristan.southwell@hotmail.com>
2017-11-15 21:55:29 +1100

    arrays challenge

---
+++ ArrayChallenge/src/com/Tristan/Main.java
@@ -0,0 +1,57 @@
+package com.Tristan;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int [] unsortedIntegers = getIntegers(5);
+        int[] sorted = sortIntegers(unsortedIntegers);
+        printArray(sorted);
+    }
+
+    public static int[] getIntegers(int number) {
+        System.out.println("Enter " + number + " integer values\r");
+        int[] values = new int[number];
+
+        for(int i=0; i<values.length; i++) {
+            values[i] = scanner.nextInt();
+        }
+
+        return values;
+    }
+
+    public static int[] printArray(int[] array) {
+       for(int i=0; i<array.length; i++) {
+           System.out.println("Element " + i + " contents " + array[i]);
+       }
+       return null;
+    }
+
+    public static int[] sortIntegers(int[] array) {
+//        int[] sortedArray = new int[array.length];
+//        for(int i=0; i<array.length; i++) {
+//            sortedArray[i] = array[i];
+//        }
+        int[] sortedArray = Arrays.copyOf(array, array.length);
+
+        boolean flag = true;
+        int temp;
+        while(flag) {
+            flag = false;
+            for(int i=0; i<sortedArray.length -1; i++) {
+                if(sortedArray[i] < sortedArray[i+1]) {
+                    temp = sortedArray[i];
+                    sortedArray[i] = sortedArray[i+1];
+                    sortedArray[i+1] = temp;
+                    flag = true;
+                }
+            }
+        }
+        return sortedArray;
+    }
+}
