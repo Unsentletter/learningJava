b6e43c3157329e4f51b6a8f8d0ac0ba2796ca3a6
Tristan Southwell <tristan.southwell@hotmail.com>
2017-12-02 13:21:46 +1100

    some random study stuff

--- textBookStudy/testingStuff/src/com/Tristan/Main.java
+++ textBookStudy/testingStuff/src/com/Tristan/Main.java
@@ -2,7 +2,28 @@ package com.Tristan;
 
 public class Main {
 
+    int noOfWatts;
+    boolean indicator;
+    String location;
+
+    public Main(int noOfWatts, boolean indicator, String site) {
+        String Location;
+
+        this.noOfWatts = noOfWatts;
+        this.indicator = indicator;
+        location = site;
+        this.superfluous();
+        superfluous();
+    }
+
+    public void superfluous() {
+        System.out.printf("Current Object: %s%n", this);
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        Main main = new Main(100, true, "loft");
+        System.out.println("No. of watts: " + main.noOfWatts);
+        System.out.println("Indicator: " + main.indicator);
+        System.out.println("Location: " + main.location);
     }
 }
